86262d307404cfbd848d9775279d75882068bedd
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2025-05-28 06:47:28 -0500

    Can now color compare tolerance on specific rgb value

--- Extensions/ColorExtensions.cs
+++ Extensions/ColorExtensions.cs
@@ -8,5 +8,11 @@
                    Math.Abs(c1.G - c2.G) <= tolerance &&
                    Math.Abs(c1.B - c2.B) <= tolerance;
         }
+        public static bool IsColorMatch(this Color c1, Color c2, int redTolerance, int greenTolerance, int blueTolerance)
+        {
+            return Math.Abs(c1.R - c2.R) <= redTolerance &&
+                   Math.Abs(c1.G - c2.G) <= greenTolerance &&
+                   Math.Abs(c1.B - c2.B) <= blueTolerance;
+        }
     }
 }
